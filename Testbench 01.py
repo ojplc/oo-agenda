2856e7bd899cc91695dd391c67459b255703d318
João Pedro Lopes da Cruz <103763209+ojplc@users.noreply.github.com>
2024-11-26 13:58:09 -0300

    Implementação de menu e adição de pessoas 💁

--- Testbench 01.py
+++ Testbench 01.py
@@ -1,3 +1,4 @@
+from packages.Agenda import Agenda
 from packages.Aluno import Aluno
 from packages.Professor import Professor
 
@@ -8,3 +9,6 @@ print(Aluno1.permissao())
 Professor1 = Professor("alan",334,1192)
 
 print(Professor1.permissao())
+
+
+agenda0 = Agenda(Professor1)
--- packages/Agenda.py
+++ packages/Agenda.py
@@ -1,8 +1,14 @@
+from packages.Aluno import Aluno
+from packages.Professor import Professor
 class Agenda:
-    def __init__(self):
+    def __init__(self, usuario_atual):
+        if not isinstance(usuario_atual, (Aluno, Professor)):
+            raise ValueError("usuario_atual deve ser uma instância de Aluno ou Professor")
         self.eventos = []
         self.pessoas = []
         self.canal = None #adicionar canal
+        self._usuario = usuario_atual
+        self.menu()
     
 
     def criar_evento(self):
@@ -10,13 +16,65 @@ class Agenda:
 
 
     def criar_pessoa(self): #comecarei pelo desenvolvimento de pessoa
-        pass
+        if self.checar_permissao():
+            atribuicao = None
+            nome = None
+            matricula = None
+            contato = None
+            while atribuicao == None:
+                print("Qual a atribuição do usuário?")
+                print("")
+                print("1. Aluno")
+                print("")
+                print("2. Professor")
+                print('')
+                atribuicao = int(input())
+                if atribuicao != 1 and atribuicao != 2:
+                    print("Selecione uma opção válida")
+                    atribuicao = None
+
+            while nome == None:
+                print("Qual o nome do usuário?")
+                print("")
+                nome = str(input())
+
+            while matricula == None:
+                print("Qual a matrícula do usuário?")
+                print("")
+                matricula = str(input())
+            
+            while contato == None:
+                print("Qual o contato do usuário?")
+                print("")
+                contato = str(input())
+            
+            print(f"O {atribuicao} chamado {nome}, com matrícula {matricula} e contato {contato} será adicionado ao sistema")
+            #atribuição ta imprimindo numero
+            #quero adicionar a opção de mudar algum atributo quando chegar aqui
+        else:
+            print("Usuário não tem as permissões necessárias")
+            
+
 
     def checar_permissao(self):
-        pass
+        return self._usuario.permissao()
 
     def cadastrar_evento(self):
         pass
 
     def menu(self):
-        pass
+        print("Bem vindo a agenda, o que você gostaria de fazer?")
+        print('')
+        print("1. Criar evento")
+        print("")
+        print("2. Adicionar usuário")
+        print("")
+        print("3. Cadastrar evento")
+        print("")
+        escolha = int(input())
+        if escolha == 1:
+            pass
+        elif escolha == 2:
+            self.criar_pessoa()
+        elif escolha == 3:
+            pass
